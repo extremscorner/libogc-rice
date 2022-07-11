872febedf9e0be9389bebb01317bc697004361e5
Extrems <metaradil@gmail.com>
2022-07-11 18:15:56 -0400

    Deassert reset on DVD_Init. Fix interrupt handling during reset. Support pausing/resuming queue.

--- gc/ogc/dvd.h
+++ gc/ogc/dvd.h
@@ -236,6 +236,7 @@ struct _dvdfileinfo {
  */
 void DVD_Init();
 void DVD_Pause();
+void DVD_Resume();
 
 
 /*! 
@@ -247,6 +248,7 @@ void DVD_Pause();
  * \return none
  */
 void DVD_Reset(u32 reset_mode);
+u32 DVD_ResetRequired();
 
 
 /*! 
