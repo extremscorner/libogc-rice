90173b8fe183e4f6859fdb97b9f6484eee6965b0
Extrems <metaradil@gmail.com>
2025-06-03 14:25:10 -0400

    Apply another workaround for old loaders. Fix arena1override example.

--- libogc/system.c
+++ libogc/system.c
@@ -186,7 +186,7 @@ extern u8 __Arena2Lo[], __Arena2Hi[];
 extern u8 __ipcbufferLo[], __ipcbufferHi[];
 #endif
 
-u8 *__argvArena1Lo = (u8*)0xdeadbeef;
+void *__argvArena1Lo = (void*)0xdeadbeef;
 
 static u32 _dsp_initcode[] =
 {
@@ -367,9 +367,11 @@ void *__attribute__((weak)) __myArena2Hi = NULL;
 
 static void __sysarena_init()
 {
-	if (__argvArena1Lo != (u8*)0xdeadbeef) __myArena1Lo = __argvArena1Lo;
+	if (__myArena1Lo == NULL && __argvArena1Lo != (void*)0xdeadbeef)
+		__myArena1Lo = __argvArena1Lo;
 #if defined(HW_DOL)
-	if (__myArena1Lo == NULL) __myArena1Lo = *(void**)0x80000030;
+	if (__myArena1Lo == NULL && *(void**)0x800000F4 != NULL)
+		__myArena1Lo = *(void**)0x80000030;
 	if (__myArena1Hi == NULL) __myArena1Hi = *(void**)0x80000038;
 	if (__myArena1Hi == NULL) __myArena1Hi = *(void**)0x800000EC;
 	if (__myArena1Hi == NULL) __myArena1Hi = *(void**)0x80000034;
