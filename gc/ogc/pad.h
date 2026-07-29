8f71163571163772e232fe431e1d986a1ccc9ce0
Extrems <metaradil@gmail.com>
2026-07-28 22:14:16 -0400

    Add PAD_GetAnalogMode. Change order of SPECCallback function arguments.

--- gc/ogc/pad.h
+++ gc/ogc/pad.h
@@ -105,6 +105,7 @@ void PAD_SetAnalogMode(u32 mode);
 void PAD_SetSamplingRate(u32 samplingrate);
 void PAD_SetSpec(u32 spec);
 void PAD_GetOrigin(PADStatus *origin);
+u32 PAD_GetAnalogMode();
 u32 PAD_GetSpec();
 u32 PAD_GetType(s32 chan,u32 *type);
 u32 PAD_IsBarrel(s32 chan);
